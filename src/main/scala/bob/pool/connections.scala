c21204a91f8ab4d9e097f2f46933128fbba4da5b
Kim Altintop <kim.altintop@gmail.com>
2012-08-19 22:07:12 +0200

    Add a round-robin pool selection strategy

--- src/main/scala/bob/pool/connections.scala
+++ src/main/scala/bob/pool/connections.scala
@@ -12,12 +12,29 @@ trait HostConnectionPools {
     import scala.util.Random
     import ResourcePool._
 
-    def RandomPool[C](pools: Map[(String, Int), Pool[C]])
+    type Host = (String, Int)
+
+    // randomly select the host connection. may block.
+    def RandomPool[C](pools: Map[Host, Pool[C]])
     : HostConnectionPool[C] = new HostConnectionPool[C] {
         private[this] val ps = pools.values.toIndexedSeq
 
         def withConnection[X](act: C => X): X =
             withResource(ps(Random.nextInt(ps.size)))(act)
+
+        def destroy() = pools.values.map(destroyAll)
+    }
+
+    // round-robin over the hosts. may block.
+    def RoundRobinPool[C](pools: Map[Host, Pool[C]])
+    : HostConnectionPool[C] = new HostConnectionPool[C] {
+        @volatile private[this] var i = 0
+        private[this] val ps = pools.values.toIndexedSeq
+
+        def withConnection[X](act: C => X): X = {
+            i = (i + 1) % ps.size
+            withResource(ps(i))(act)
+        }
 
         def destroy() = pools.values.map(destroyAll)
     }
